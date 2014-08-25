e7f29f944e61e0860d21a75483d97878d3329ee6
Dominique Busser <dombusser@gmail.com>
2014-08-25 14:46:47 +0200

    securesocial savegame

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,8 +3,10 @@ package controllers;
 import models.Attribute;
 import models.Parameter;
 import models.Problem;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
+import securesocial.*;
 import views.html.*;
 
 public class Application extends Controller {
@@ -18,7 +20,7 @@ public class Application extends Controller {
 		return ok(define.render());
 	}
 
-	public static Result index() {
+	public static Result fucker() {
 
 		Problem p = new Problem();
 		Parameter kosten = new Parameter().setId(1).setName("Kosten");
@@ -44,4 +46,86 @@ public class Application extends Controller {
 
 		return ok(index.render(p));
 	}
+
+	public static Logger.ALogger logger = Logger
+			.of("application.controllers.Application");
+	private RuntimeEnvironment env;
+
+	/**
+	 * A constructor needed to get a hold of the environment instance. This
+	 * could be injected using a DI framework instead too.
+	 * 
+	 * @param env
+	 */
+	public Application(RuntimeEnvironment env) {
+		this.env = env;
+	}
+
+	/**
+	 * This action only gets called if the user is logged in.
+	 * 
+	 * @return
+	 */
+
+	@SecuredAction
+	public Result index() {
+		if (logger.isDebugEnabled()) {
+			logger.debug("access granted to index");
+		}
+		DemoUser user = (DemoUser) ctx().args.get(SecureSocial.USER_KEY);
+		return ok(index.render(user, SecureSocial.<DemoUser> env()));
+	}
+
+	@UserAwareAction
+	public Result userAware() {
+		DemoUser demoUser = (DemoUser) ctx().args.get(SecureSocial.USER_KEY);
+		String userName;
+		if (demoUser != null) {
+			BasicProfile user = demoUser.main;
+			if (user.firstName().isDefined()) {
+				userName = user.firstName().get();
+			} else if (user.fullName().isDefined()) {
+				userName = user.fullName().get();
+			} else {
+				userName = "authenticated user";
+			}
+		} else {
+			userName = "guest";
+		}
+		return ok("Hello " + userName + ", you are seeing a public page");
+	}
+
+	@SecuredAction(authorization = WithProvider.class, params = { "twitter" })
+	public Result onlyTwitter() {
+		return ok("You are seeing this because you logged in using Twitter");
+	}
+
+	@SecuredAction
+	public Result linkResult() {
+		DemoUser current = (DemoUser) ctx().args.get(SecureSocial.USER_KEY);
+		return ok(linkResult.render(current, current.identities));
+	}
+
+	/**
+	 * Sample use of SecureSocial.currentUser. Access the /current-user to test
+	 * it
+	 */
+	public F.Promise<Result> currentUser() {
+		return SecureSocial.currentUser(env).map(
+				new F.Function<Object, Result>() {
+					@Override
+					public Result apply(Object maybeUser) throws Throwable {
+						String id;
+
+						if (maybeUser != null) {
+							DemoUser user = (DemoUser) maybeUser;
+							id = user.main.userId();
+						} else {
+							id = "not available. Please log in.";
+						}
+						return ok("your id is " + id);
+					}
+				});
+	}
+
 }
--- build.sbt
+++ build.sbt
@@ -2,10 +2,15 @@ name := "morphBMC"
 
 version := "1.0-SNAPSHOT"
 
+resolvers += Resolver.sonatypeRepo("releases")
+
+//resolvers += "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"
+
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
-  cache
+  cache,
+  "ws.securesocial" %% "securesocial" % "2.1.4"
 )     
 
 play.Project.playJavaSettings
