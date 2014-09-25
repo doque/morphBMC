2ea3de03e67cdcbbf6fc1bf911b3329700610166
Dominique Busser <dombusser@gmail.com>
2014-09-25 18:07:56 +0200

    Added Results View to browse all possible configurations

--- public/js/ResultsController.js
+++ public/js/ResultsController.js
@@ -1,25 +1,60 @@
 morphBMC.controller("ResultsController", ['$scope', '$http', '$filter', function($scope, $http) {
 
+	$scope.parameters = [];
+	$scope.attributes = [];
+	$scope.configurations = [];
 
-	$scope.allPossible() = function() {
+	$scope.allPossible = function() {
+		$scope.configurations = getAllCombinations($scope.attributes);
 
-		var all = [];
-		for (var i=0; i<$scope.parameters.length; i++) {
-			var p = $scope.parameters[i];
-			
-		}
+	};
 
-		// data structure:
-		// [{
-		// 		param1: attr2,
-		// 		param2: attr1,
-		// 		param3: attr4
-		// }, {
-		// 		...
-		// }]
+	/**
+	 * returns the parameter that a single attribute belongs to
+	 * @param id - the attribute id
+	 */
+	$scope.getParameterByAttribute = function(id) {
+		// walk through parameters
+		var param = null;
+		angular.forEach($scope.parameters, function(p) {
+			// walk through all attributes of parameters
+			angular.forEach(p.attributes, function(a) {
+				if (a.id == id) {
+					param = p;
+				}
+			});
+		});
+		return param;
 	};
 
 
+	function getAllCombinations(arraysToCombine) {
+	    var divisors = [];
+	    for (var i = arraysToCombine.length - 1; i >= 0; i--) {
+	       divisors[i] = divisors[i + 1] ? divisors[i + 1] * arraysToCombine[i + 1].length : 1;
+	    }
+	    
+	    function getPermutation(n, arraysToCombine) {
+	       var result = [], 
+	           curArray;
+	       for (var i = 0; i < arraysToCombine.length; i++) {
+	          curArray = arraysToCombine[i];
+	          result.push(curArray[Math.floor(n / divisors[i]) % curArray.length]);
+	       }    
+	       return result;
+	    }
+	    
+	    var numPerms = arraysToCombine[0].length;
+	    for(var i = 1; i < arraysToCombine.length; i++) {
+	        numPerms *= arraysToCombine[i].length;
+	    }
+	    
+	    var combinations = [];
+	    for(var i = 0; i < numPerms; i++) {
+	        combinations.push(getPermutation(i, arraysToCombine));
+	    }
+	    return combinations;
+	}
 
 
 	// set up environment on load
@@ -28,10 +63,9 @@ morphBMC.controller("ResultsController", ['$scope', '$http', '$filter', function
 		// if present yet
 		$scope.parameters = data.problem.parameters;
 		angular.forEach($scope.parameters, function(p) {
-			angular.forEach(p.attributes, function(a) {
-				$scope.attributes.push(a.id);
-			});
+			$scope.attributes.push(p.attributes);
 		});
+		$scope.allPossible();
 
 		$http.get("/api/problems/" + window.PROBLEM_ID+ "/compatibilities").success(function(data) {
 			$scope.compatibilities = data.compatibilities;
