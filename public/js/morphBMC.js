5688a011ac3628865fee0bb08d0843b1d55974cd
Dominique Busser <dombusser@gmail.com>
2014-10-24 13:11:52 +0200

    attributes can be added by pressing return

--- public/js/morphBMC.js
+++ public/js/morphBMC.js
@@ -73,4 +73,15 @@ var morphBMC = angular.module('morphBMC', ['ngRoute', 'tableSort']).config(
             });
         }
     };
-});
+}).directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});;
