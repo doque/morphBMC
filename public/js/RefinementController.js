1b8ed790fc41ce1210539d08e8963b2bf6271ebf
Dominique Busser <dombusser@gmail.com>
2014-10-29 11:43:38 +0100

    different style for other's parameters in refinement view

--- public/js/RefinementController.js
+++ public/js/RefinementController.js
@@ -1,5 +1,81 @@
 app.controller("RefinementController", ['$scope', '$http', 'SocketService', function($scope, $http, SocketsService) {
 
+	/**
+	 * send a POST request to add a new attribute
+	 * @param {[type]} parameter which parameter the attribute belongs to
+	 */
+	$scope.addAttribute = function(parameter) {
+
+		if (!parameter.attribute) return;
+
+		if (parameter.attribute.name.length === 0) {
+			return;
+		}
+
+		$http.post("/api/problems/" + window.PROBLEM_ID + "/parameters/"+parameter.id+"/attributes", {
+			"name": parameter.attribute.name
+		}).success(function(data) {
+			parameter.attributes.unshift(data.attribute);
+			parameter.attribute.name = "";
+		});
+	};
+
+	$scope.yours = function(parameterId) {
+		var yours = false;
+		angular.forEach($scope.parameters, function(p) {
+			if (p.id === parameterId && p.userId == window.USER_ID) {
+				yours = true;
+				return;
+			}
+		});
+		return yours;
+	};
+
+	// collaborative shit
+	$scope.$on('test', function(event, args) {
+		console.log(args);
+	});
+
+	/**
+	 * send a post request to add a new parameter
+	 */
+	$scope.addParameter = function() {
+		if (!$scope.parameter) return;
+		// sanity check
+		if ($scope.parameter.name.length === 0) {
+			// no value given
+			$scope.addingParameter = false;
+			return;
+		}
+		
+		$http.post("/api/problems/" + window.PROBLEM_ID + "/parameters", {
+			"name": $scope.parameter.name
+		}).success(function(data) {
+			$scope.addingParameter = false;
+			// reset input field model
+			$scope.parameter.name = "";
+			$scope.parameters.push(data.parameter);
+		});
+	};
+
+	/**
+	 * sends a DELETE request to remove a parameter
+	 * and kicks it from the $scope
+	 */
+	$scope.removeParameter = function(parameter) {
+		$http.delete("/api/problems/" + window.PROBLEM_ID + "/parameters/" + parameter.id).success(function() {
+			// remove from scope
+			angular.forEach($scope.parameters, function(p, index) {
+				if (p.id === parameter.id) {
+					$scope.parameters.splice(index, 1);
+					return;
+				}
+			});
+
+		});
+	};
+
+
 	// set up environment on load
 	$http.get("/api/problems/"+window.PROBLEM_ID+"/parameters?all=yes").success(function(data) {
 		$scope.parameters = data.parameters;
